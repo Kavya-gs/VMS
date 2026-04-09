21119dd917ac624f1dc3f5b8c8090e3e0c6f521b
kavya-gs <kavya.jaimini@geminisolutions.com>
2026-04-09 20:14:04 +0530

    fix: some changes

--- server/src/controllers/visitor.controller.js
+++ server/src/controllers/visitor.controller.js
@@ -6,16 +6,10 @@ import QRCode from "qrcode";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  port: 587,
-  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-  connectionTimeout: 10000,
+  }
 });
 
 const sendEmail = async (to, subject, html, attachments = []) => {
